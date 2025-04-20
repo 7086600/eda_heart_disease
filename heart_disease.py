af890e67e30d3fa961eb24be303be2d6d29bd878
AK <67439739+7086600@users.noreply.github.com>
2025-04-20 22:35:10 +0300

    Add column names for heart disease dataset and update data loading

--- heart_disease.py
+++ heart_disease.py
@@ -1,4 +1,23 @@
 import pandas as pd
 
-heart = pd.read_csv('./heart+disease/processed.cleveland.data')
+# column names for the dataset
+# The dataset is from the UCI Machine Learning Repository
+# https://archive.ics.uci.edu/ml/datasets/heart+Disease
+column_names = ['age', 
+                'sex', 
+                'cp', 
+                'trestbps', 
+                'chol', 
+                'fbs', 
+                'restecg', 
+                'thalach', 
+                'exang', 
+                'oldpeak', 
+                'slope', 
+                'ca', 
+                'thal', 
+                'num']
+
+heart = pd.read_csv('./heart+disease/processed.cleveland.data', names=column_names)
 print(heart.head())
+print(heart.shape)
